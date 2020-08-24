7a9d2798d065889ad89deb261214e3aeea67f3e6
dinaAlsaid <dinaAlsaid@github.com>
2020-08-24 16:26:26 +0300

    feature 2 done

--- js/app.js
+++ js/app.js
@@ -7,46 +7,50 @@ $.ajax('../data/page-1.json')
     .then(data => {
         let $select = $('select');
         data.forEach(ele => {
-            new Animal (ele.image_url, ele.title, ele.description, ele.keyword, ele.horns);
+            new Animal(ele.image_url, ele.title, ele.description, ele.keyword, ele.horns);
         });
         console.log(animalObjArr);
         animalObjArr.forEach(item => {
             item.render();
-            if(keywordOpt.includes(item.keyword) === false){
+            if (keywordOpt.includes(item.keyword) === false) {
                 keywordOpt.push(item.keyword);
                 $select.append(`<option value="${item.keyword}">${item.keyword}</option>`);
-              
+
 
             }
         })
         console.log(keywordOpt);
-        $select.click(function(event){
+        $select.click(function (event) {
             animalObjArr.forEach(item => {
-                if (item.keyword === event.target.value){
-                    $(`.${item.keyword}`).show();
+                if (event.target.value !== 'default') {
+                    if (item.keyword === event.target.value) {
+                        $(`.${item.keyword}`).show();
+                    } else {
+                        $(`.${item.keyword}`).hide();
+                    }
                 } else {
-                    $(`.${item.keyword}`).hide();  
+                    $(`.${item.keyword}`).show();
                 }
+
             })
             console.log(event.target.value);
-    })
+        })
     });
-// $('section')
 
-function Animal(image_url, title, description, keyword, horns){
+function Animal(image_url, title, description, keyword, horns) {
     this.image_url = image_url;
     this.title = title;
     this.description = description;
     this.keyword = keyword;
     this.horns = horns;
-    
+
     animalObjArr.push(this);
 }
 
-Animal.prototype.render = function(){
+Animal.prototype.render = function () {
     let section = $('.photo-template').first().clone();
     section.find('h2').text(this.title);
-    section.find('img').attr('src',this.image_url);
+    section.find('img').attr('src', this.image_url);
     section.find('p').text(this.description);
     section.addClass(this.keyword);
     $('main').append(section);
